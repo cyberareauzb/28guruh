4f15c67adb9f45139b10992d618780180f2b41cb
Bobur Xamidov <khamidoffb@gmail.com>
2025-07-26 08:39:45 +0500

    ABC added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   axios.defaults.headers.common["Content-Type"] = "application/json"
   axios.defaults.headers.common.Authorization = "Bearer " + localStorage.getItem('token')
 
+  let abc = 159
 
   let navigate = useNavigate()
 
